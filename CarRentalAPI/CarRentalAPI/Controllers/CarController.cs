d407d60e4849ffa036943e528baaeb88f1a36446
Stan Bubbers <stan.bubbers@live.co.uk>
2021-07-20 22:23:26 +0100

    add: controller with initial endpoints

---
+++ CarRentalAPI/CarRentalAPI/Controllers/CarController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Car>>> GetCars()
+        {
+      
+        }
+
+        [Route("rented")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RentedCar>>> GetRentedCars()
+        {
+
+        }
+
+        [Route("rented/check")]
+        [HttpGet]
+        public async Task<ActionResult<string>> CheckRentedCarExpiry()
+        {
+
+        }
+
+
+        [HttpPut("rent/{id}/{rentPeriod}")]
+        public async Task<ActionResult<string>> RentCar(int id, int rentPeriod)
+        {
+
+        }
+
+        [HttpPut("return/{id}")]
+        public async Task<ActionResult<string>> ReturnCar(int id)
+        {
+
+        }
+    }
+}
