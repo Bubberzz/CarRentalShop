d4cb13693d8a9a08d8020d9c6b21c01a87525a64
Stan Bubbers <stan.bubbers@live.co.uk>
2021-07-20 22:39:41 +0100

    add: empty db for rented cars

---
+++ CarRentalAPI/CarRentalAPI/Data/RentedCarDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CarRentalAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentalAPI.Data
+{
+    public class RentedCarDbContext : DbContext
+    {
+        public DbSet<RentedCar> RentedCars { get; set; }
+
+        public RentedCarDbContext(DbContextOptions<RentedCarDbContext> options) : base(options)
+        {
+        }
+    }
+}
